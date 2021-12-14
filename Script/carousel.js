ef4aee616b03c3f843a735b02f408580ee935089
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2021-12-14 21:18:33 +0530

    start carousel part

---
+++ Script/carousel.js
@@ -0,0 +1,11 @@
+
+let slide = document.querySelectorAll(".img_container");
+
+let counter = 1;
+let slideSize = '1440px';
+
+slide.style.transform = "translateX(" + (-slideSize * counter) + "px)";
+
+rightArrow.addEventListener("click",()=>{
+    
+})
